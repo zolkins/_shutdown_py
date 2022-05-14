188f01139b0a837a02a3af26d6e96b87e8b4ff55
zolkins <tester.zolkin@gmail.com>
2022-05-14 22:23:34 +0300

    icon

--- start.py
+++ start.py
@@ -148,7 +148,8 @@ class Shutdown:
             root.geometry("300x80")
             self.i = 0
     
-
+ico = path0("ico.png")
+root.iconphoto(True, tk.PhotoImage(file=ico))
 root = Tk()
 Shutdown(root)
 root.mainloop()
