fb2d69a14121b30ac753a1d84585774cb2415ce3
zolkins <tester.zolkin@gmail.com>
2022-05-14 20:39:23 +0300

    add path

--- start.py
+++ start.py
@@ -7,7 +7,12 @@ import tkinter as tk
 import tkinter.ttk as ttk 
 from tkinter import colorchooser
 
- 
+def path0(relative_path):
+    base_path = getattr(
+        sys,
+        '_MEIPASS',
+        os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
 
 class Shutdown:
  
